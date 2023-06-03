2c0bf6b60f9cd0f9df8daf24f796fb46284d95fa
gludovatza <attila@gludovatz.hu>
2023-06-03 10:27:24 +0200

    Remove username from register form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -17,11 +17,11 @@
             </div>
 
             <!-- Username -->
-            <div class="mt-4">
+            {{-- <div class="mt-4">
                 <x-input-label for="username" :value="__('Username')" />
                 <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required />
                 <x-input-error :messages="$errors->get('username')" class="mt-2" />
-            </div>
+            </div> --}}
 
             <!-- Email Address -->
             <div class="mt-4">
